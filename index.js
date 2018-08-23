d47476aa2889a9ce31695f01896888b27011be69
Ville-Matti Hakanpää <villematti@villematti.info>
2018-08-24 00:34:17 +0300

    Testing vhost

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const vhost = require('vhost')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log(req)
-    if(req.headers.host === 'talviruusu.com') {
-        res.send('hello talviruusu')
-    } else if(req.headers.host === 'siivousville.com') {
-        req.send('hello siivousville')
-    }
-})
+function createVirtualHost(domainName, dirPath) {
+    return vhost(domainName, express.static( dirPath ));
+}
 
-app.listen(80, () => console.log("Server running"))
+//Create the virtual hosts
+var trHost = createVirtualHost("talviruusu.com", "talviruusu");
+var svHost = createVirtualHost("siivousville.com", "siivousville");
+
+//Use the virtual hosts
+app.use(vhost('talviruusu.com', (req, res) => {
+    console.log(req)
+    res.send('Hello from talviruusu')
+}));
+app.use(vhost('siivousville.com', (req, res) => {
+    console.log(req)
+    res.send('Hello from siivousville')
+}));
+
+//Start server
+var port = 80;
+app.listen( port, function() {
+    console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
+});
