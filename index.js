595eb6611688032a437441c673577cfb5e98f0ed
Ville-Matti Hakanpää <villematti@villematti.info>
2018-08-24 00:08:35 +0300

    Add console to see req

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+    console.log(req)
     if(req.headers.host === 'talviruusu.com') {
         res.send('hello talviruusu')
     } else if(req.headers.host === 'talviruusu.com') {
